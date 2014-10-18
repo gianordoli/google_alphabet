df6fa4428b0738b7655de7f22359c28fb97970ab
gianordoli <gianordoligabriel@yahoo.com.br>
2014-10-18 16:38:26 -0400

    Added googleSuggest library
    Now searching from different google services

--- js/main.js
+++ js/main.js
@@ -1,35 +1,5 @@
-var suggestCallBack; // global var for autocomplete jsonp
-
 $(document).ready(function () {  
 
-    $("#search").autocomplete({
-        source: function(request, response) {
-            $.getJSON("http://suggestqueries.google.com/complete/search?callback=?",
-                { 
-                  "hl":"en", // Language                  
-                  "jsonp":"suggestCallBack", // jsonp callback function name
-                  "q":request.term, // query term
-                  "client":"youtube" // force youtube style response, i.e. jsonp
-                }
-            );
-            suggestCallBack = function (data) {
-                var suggestions = [];
-                $.each(data[1], function(key, val) {
-                    suggestions.push({"value":val[0]});
-                });
-                suggestions.length = 5; // prune suggestions list to only 5 items
-                response(suggestions);
-            };
-        },
-        select: function(event, ui) {
-            var query = ui.item.value;
-            console.log(query);
-        },        
-    });
+    $('#search').googleSuggest({ service: 'youtube' });
 
-    $("#search").keypress(function(e){
-        if (e.keyCode == 13) {
-            console.log($(this).val());
-        }
-    });
 });
