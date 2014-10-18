9800737476d7b1f56faadfb7e5b4d14cb2a258b0
gianordoli <gianordoligabriel@yahoo.com.br>
2014-10-18 18:11:59 -0400

    Custom Search + Autocomplete

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 var goSearch;
 
-$(document).ready(function () {  
+$(document).ready(function () {
 
-    // Set the default service to web (checked radio button)
-    $('#search').googleSuggest({ service: $('input[name="service"]').val() });
+    function googleSearch(callback) {
+        var cx = '009093787028265469982:75wos-7sdjk';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+          '//www.google.com/cse/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+
+        callback();
+    }
+
+    function setAutocomplete() {
+        console.log('Called setAutocomplete.');
+        if($('input[name="search"]').length == 0){
+            console.log('Search bar not found.');
+            setTimeout(setAutocomplete, 1000);
+        }else{
+            console.log('Search bar found.');
+            // Set the default service to web (checked radio button)
+            $('input[name="search"]').googleSuggest({ service: $('input[name="service"]').val() });
+        }
+    }
+
+    googleSearch(setAutocomplete);
 
     // Attribute Google Search service when radio button is selected
     $('input[name="service"]').click(function(){
         // console.log($(this).val());
-        $('#search').googleSuggest({ service: $(this).val() });
+        $('input[name="search"]').googleSuggest({ service: $(this).val() });
     });
 
     goSearch = function(query){
